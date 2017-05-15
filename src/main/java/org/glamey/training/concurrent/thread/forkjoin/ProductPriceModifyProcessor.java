cd6bcca160c29c92edb572804290fd664bfe4add
zhouyang.zhou <zy_8516@163.com>
2017-05-15 11:53:19 +0800

    修改变量

--- src/main/java/org/glamey/training/concurrent/thread/forkjoin/ProductPriceModifyProcessor.java
+++ src/main/java/org/glamey/training/concurrent/thread/forkjoin/ProductPriceModifyProcessor.java
@@ -12,36 +12,36 @@ public class ProductPriceModifyProcessor extends RecursiveAction {
 
   private static final int THRESHOLD = 20;
   private List<Product> products;
-  private int startId;
-  private int endId;
+  private int low;
+  private int high;
 
-  public ProductPriceModifyProcessor(List<Product> products, int startId, int endId) {
+  public ProductPriceModifyProcessor(List<Product> products, int low, int high) {
     this.products = products;
-    this.startId = startId;
-    this.endId = endId;
+    this.low = low;
+    this.high = high;
   }
 
   @Override protected void compute() {
 
-    if(endId - startId < THRESHOLD) {
-      doInner(products, startId, endId);
+    if(high - low < THRESHOLD) {
+      doInner(products, low, high);
       return;
     }
 
-    int middle = (endId + startId) >>> 1;
-    ProductPriceModifyProcessor processor1 = new ProductPriceModifyProcessor(products, startId, middle);
-    ProductPriceModifyProcessor processor2 = new ProductPriceModifyProcessor(products, middle, endId);
+    int middle = (high + low) >>> 1;
+    ProductPriceModifyProcessor processor1 = new ProductPriceModifyProcessor(products, low, middle);
+    ProductPriceModifyProcessor processor2 = new ProductPriceModifyProcessor(products, middle, high);
     invokeAll(processor1, processor2);
     processor1.join();
     processor2.join();
   }
 
-  private void doInner(List<Product> products, int startId, int endId) {
-    for (int i = startId; i < endId; i++) {
+  private void doInner(List<Product> products, int low, int high) {
+    for (int i = low; i < high; i++) {
       Product product = products.get(i);
       product.setPrice(0.1 * product.getPrice());
     }
     Thread thread = Thread.currentThread();
-    System.out.println(String.format("thread[id=%d,name=%s],range=[%d-%d)", thread.getId(), thread.getName(), startId, endId));
+    System.out.println(String.format("thread[id=%d,name=%s],range=[%d-%d)", thread.getId(), thread.getName(), low, high));
   }
 }
