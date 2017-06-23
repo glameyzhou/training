599156ccf7ee8f00b2c09619b17da0ba91ae60f3
zhouyang.zhou <zy_8516@163.com>
2017-06-23 18:56:21 +0800

    三种方式实现生产消费模型

--- src/main/java/org/glamey/training/designmodel/producer_consumer/produce_consume_3/Demo3.java
+++ src/main/java/org/glamey/training/designmodel/producer_consumer/produce_consume_3/Demo3.java
@@ -1,12 +1,15 @@
 package org.glamey.training.designmodel.producer_consumer.produce_consume_3;
 
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
 /**
  * @author zhouyang.zhou. 2017.06.22.11.
  */
 public class Demo3 {
 
     public static void main(String[] args) {
-        /*BlockingQueue<String> queue = new LinkedBlockingQueue<>(2);
+        BlockingQueue<String> queue = new LinkedBlockingQueue<>(2);
 
         Demo3Producer producer = new Demo3Producer(queue);
         Demo3Consumer consumer = new Demo3Consumer(queue);
@@ -18,22 +21,5 @@ public class Demo3 {
         for (int i = 0; i < 10; i++) {
             new Thread(consumer, "c_" + i).start();
         }
-
-
-        Executors.newFixedThreadPool(10);
-        Executors.newSingleThreadExecutor();
-        Executors.newCachedThreadPool();*/
-
-
-        String s1 = "abc";
-        String s2 = "abc";
-        String s3 = "ab" + "c";
-        String s4 = new String("abc");
-        String s5 = s1;
-        System.out.println(s1 == s2);
-        System.out.println(s2 == s3);
-        System.out.println(s1 == s4);
-        System.out.println(s5 == s1);
-
     }
 }
