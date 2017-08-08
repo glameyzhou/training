069c21efd3ccfa2fd6dea70700cff1aea304c17a
zhouyang.zhou <zy_8516@163.com>
2017-08-08 12:57:57 +0800

    根据指定的高度打印菱形rhombus

---
+++ src/main/java/org/glamey/training/algorithm/leetcode/RhombusDemo.java
@@ -0,0 +1,78 @@
+package org.glamey.training.algorithm.leetcode;
+
+/**
+ *
+ * print the rhombus
+ *
+ *       *
+ *      ***
+ *     *****
+ *      ***
+ *       *
+ *
+ * @author zhouyang.zhou. 2017.08.08.11.
+ */
+public class RhombusDemo {
+
+
+  /**
+   * print the rhombus for special high
+   * @param high
+   */
+  public static void process(int high) {
+    if(high < 3 || high % 2 == 0) {
+      System.out.println("the high must greater than 3");
+      return;
+    }
+
+    System.out.println("<--------------[" + high + "]-------------->");
+
+    int middle = (high >> 1) + 1;
+    int maxStar = high;
+
+    int blankCount, starCount;
+
+    for (int i = 1; i <= high; i++) {
+      //middle
+      if(i == middle) {
+        starCount = maxStar;
+        blankCount = 0;
+      }
+
+      //up
+      else if(i < middle) {
+        starCount = (i << 1) - 1;
+        blankCount = maxStar - starCount;
+      }
+
+      //down
+      else {
+        int upLineNum = (high - i) + 1;
+        starCount = (upLineNum << 1) - 1;
+        blankCount = maxStar - starCount;
+      }
+      printStar(starCount, blankCount);
+    }
+  }
+
+  private static void printStar(int starCount, int blankCount) {
+    int halfBlankCount = blankCount >> 1;
+    int lastStar = halfBlankCount + starCount;
+    int max = starCount + blankCount;
+
+    for (int i = 1; i <= max ; i++) {
+      if(i <= halfBlankCount || i > lastStar) {
+        System.out.print(" ");
+      } else {
+        System.out.print("*");
+      }
+    }
+    System.out.println();
+  }
+
+  public static void main(String[] args) {
+    process(13);
+    process(5);
+  }
+
+}
