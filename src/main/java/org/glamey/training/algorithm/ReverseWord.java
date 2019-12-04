3ed9e1586f52720423cad14e6fe35644f99b8efc
yang.zhou <>
2019-12-04 14:26:23 +0800

    字符串反转（双指针操作）

--- src/main/java/org/glamey/training/algorithm/ReverseWord.java
+++ src/main/java/org/glamey/training/algorithm/ReverseWord.java
@@ -3,35 +3,53 @@ package org.glamey.training.algorithm;
 import java.util.Stack;
 
 /**
- * 字符根据空间进行翻转
+ * 字符根据空格进行翻转
  *
  * @author zhouyang.zhou. 2017.05.09.15.
  */
 public class ReverseWord {
 
-  public static void main(String[] args) {
-    methodA();
-  }
+    public static void main(String[] args) {
+        methodA();
 
-  private static void methodA() {
-    String string = "hello world java";
-    Stack<String> stack = new Stack<>();
-    char[] chars = string.toCharArray();
-    StringBuilder builder = new StringBuilder(20);
-    for (char aChar : chars) {
-      boolean whitespace = Character.isWhitespace(aChar);
-      if(whitespace) {
-        if(builder.length() > 0) {
-          stack.push(builder.toString());
-          builder.setLength(0);
+        int[] array = {0, 0, 1, 1, 2, 3, 3, 4, 5, 5};
+        int len = removeDuplicate(array);
+        for (int i = 0; i <= len; i++) {
+            System.out.println(array[i]);
         }
-      } else {
-        builder.append(aChar);
-      }
     }
 
-    while (!stack.isEmpty()) {
-      System.out.println(stack.pop());
+    public static int removeDuplicate(int[] array) {
+        int index = 0;
+        for (int i = 1; i < array.length; i++) {
+            if (array[index] != array[i]) {
+                index += 1;
+                array[index] = array[i];
+            }
+        }
+        return index;
     }
-  }
+
+    private static void methodA() {
+        String string = "hello world java";
+        Stack<String> stack = new Stack<>();
+        char[] chars = string.toCharArray();
+        StringBuilder builder = new StringBuilder(20);
+        for (char aChar : chars) {
+            boolean whitespace = Character.isWhitespace(aChar);
+            if (whitespace) {
+                if (builder.length() > 0) {
+                    stack.push(builder.toString());
+                    builder.setLength(0);
+                }
+            } else {
+                builder.append(aChar);
+            }
+        }
+
+
+        while (!stack.isEmpty()) {
+            System.out.println(stack.pop());
+        }
+    }
 }
