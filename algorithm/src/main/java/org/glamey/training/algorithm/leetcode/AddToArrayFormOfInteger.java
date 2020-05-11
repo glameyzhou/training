0c9aab004e2b39dd49fee17f972db4fd40caf68d
yang.zhou <>
2020-05-11 12:25:31 +0800

    整形数字+数组

--- algorithm/src/main/java/org/glamey/training/algorithm/leetcode/AddToArrayFormOfInteger.java
+++ algorithm/src/main/java/org/glamey/training/algorithm/leetcode/AddToArrayFormOfInteger.java
@@ -1,6 +1,5 @@
 package org.glamey.training.algorithm.leetcode;
 
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -27,49 +26,45 @@ import java.util.List;
  * 输出：[1,0,2,1]
  * 解释：215 + 806 = 1021
  * 示例 4：
- *
+ * <p>
  * 输入：A = [9,9,9,9,9,9,9,9,9,9], K = 1
  * 输出：[1,0,0,0,0,0,0,0,0,0,0]
  * 解释：9999999999 + 1 = 10000000000
  *  
- *
+ * <p>
  * 提示：
- *
+ * <p>
  * 1 <= A.length <= 10000
  * 0 <= A[i] <= 9
  * 0 <= K <= 10000
  * 如果 A.length > 1，那么 A[0] != 0
- *
+ * <p>
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/add-to-array-form-of-integer
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
  * @author yang.zhou 2020.01.06.14
  */
 public class AddToArrayFormOfInteger {
 
     public static void main(String[] args) {
-        int[] array = {1, 2, 3};
-        int k = 1877;
-        System.out.println(new AddToArrayFormOfInteger().addToArrayForm(array, k));
+        System.out.println(addToArray(new int[]{1, 2, 3}, 1877));
+        System.out.println(addToArray(new int[]{9, 9, 9}, 1));
     }
 
-    public List<Integer> addToArrayForm(int[] array, int k) {
-        LinkedList<Integer> list = new LinkedList<>();
-        int index = array.length;
-        int cur = k;
-        while (--index >= 0 || cur > 0) {
-            if (index >= 0) {
-                cur += array[index];
-            }
+    private static List<Integer> addToArray(int[] nums, int k) {
+        if (k == 0 || nums == null || nums.length == 0) return null;
 
+        LinkedList<Integer> list = new LinkedList<>();
+        int cur = k, index = nums.length - 1;
+        while (index >= 0 || cur > 0) {
+            if (index >= 0) {
+                cur += nums[index];
+            }
+            index--;
             list.offerFirst(cur % 10);
             cur /= 10;
         }
-
-//        Collections.reverse(list);
-
-        Integer[] nums = list.toArray(new Integer[0]);
-        System.out.println(Arrays.toString(nums));
         return list;
     }
 }
