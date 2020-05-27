01475908e414950726d6f7b850f28e35b68ababa
yang.zhou <>
2020-05-27 12:42:11 +0800

    算法：最大连续子序列和。（暴力+前序列和是否为0）

--- algorithm/src/main/java/org/glamey/training/algorithm/leetcode/dp/MaximumSubarray.java
+++ algorithm/src/main/java/org/glamey/training/algorithm/leetcode/dp/MaximumSubarray.java
@@ -21,9 +21,60 @@ package org.glamey.training.algorithm.leetcode.dp;
 public class MaximumSubarray {
 
 
-    public int maxSubArray(int[] nums) {
-        int n = nums.length;
-        int currentSum = nums[0];
-        return 0;
+    public static void main(String[] args) {
+        System.out.println(maxLongestSubArray(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4})); //6
+        System.out.println(greedy(new int[]{-2, -3, -1, 1, -5, 4})); //4
+    }
+
+    /**
+     * 时间复杂度O(n)
+     * 空间复杂度O(1)
+     *
+     * @param nums
+     * @return
+     */
+    private static int greedy(int[] nums) {
+        //初始化值均为第0个元素
+        int maxSum = nums[0], maxTmp = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            /**
+             * 如果当前元素之前的所有元素之和<=0,那么当前元素nums[i]应该就是最大连续子序列和
+             * 反之：继续相加
+             */
+            if (maxTmp <= 0) {
+                maxTmp = nums[i];
+            } else {
+                maxTmp += nums[i];
+            }
+
+            maxSum = Math.max(maxSum, maxTmp);
+        }
+        return maxSum;
+    }
+
+    /**
+     * 暴力破解方法
+     * 时间复杂度O(N*N)
+     *
+     * @param nums
+     * @return
+     */
+    private static int maxLongestSubArray(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        if (nums.length == 1) {
+            return nums[0];
+        }
+
+        int max = nums[0];
+        for (int i = 0; i < nums.length; i++) {
+            int sum = 0;
+            for (int j = i; j < nums.length; j++) {
+                sum += nums[j];
+                max = Math.max(sum, max);
+            }
+        }
+        return max;
     }
 }
